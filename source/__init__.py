7ff0c821e5d09bef748e958e51ecbd04554992ba
vedgewater <vedgewater@c19fddf1-0dbb-4563-8666-9d4faa189954>
2009-03-29 19:31:40 +0000

    Version increment

--- source/__init__.py
+++ source/__init__.py
@@ -3,7 +3,7 @@ def name():
 def description():
   return "A collection of tools for Anuga pre and post processing"
 def version():
-  return "Version 0.0"
+  return "Version 0.1"
 def qgisMinimumVersion():
   return "1.0"
 def authorName():
